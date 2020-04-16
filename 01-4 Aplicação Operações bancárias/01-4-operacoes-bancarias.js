9c9d2c6335370a07c324ecde4130bc837d83dbce
Rafael Martins <rafaeldcmartins@gmail.com>
2020-04-15 23:05:20 -0300

    added solution

--- 01-4 Aplicação Operações bancárias/01-4-operacoes-bancarias.js
+++ 01-4 Aplicação Operações bancárias/01-4-operacoes-bancarias.js
@@ -1,5 +1,74 @@
+// Operações bancárias
+
 const user = {
-name: 'Fulano',
-transactions: [],
-balance: 0
+    name: 'Fulano',
+    transactions: [],
+    balance: 0
 };
+
+// Adicionar transações
+function createTransaction(type, value) {
+    user.transactions.push({ type: type, value: value });
+    updateBalance(type, value);
+}
+
+// Atualizar saldo
+function updateBalance(type, value) {
+    if (type === 'credit') {
+        user.balance += value;
+    } else if (type === 'debit') {
+        user.balance -= value;
+    }
+}
+
+// Relatórios
+
+// Transação de maior valor por tipo
+function getHigherTransactionByType(type) {
+    let highTransaction;
+    let highValue = 0;
+    for (let transaction of user.transactions) {
+        if (transaction.type === type && transaction.value > highValue) {
+            highValue = transaction.value;
+            highTransaction = transaction;
+        }
+    }
+    return highTransaction;
+}
+
+// Valor médio das transações
+function getAverageTransactionValue() {
+    let average = 0;
+    for (let transaction of user.transactions) {
+        average += transaction.value;
+    }
+    return average / user.transactions.length;
+}
+
+// Número de transações de cada tipo
+function getTransactionsCount() {
+    let credit = 0;
+    let debit = 0;
+    for (let transaction of user.transactions) {
+        if (transaction.type === 'credit') {
+            credit++
+        } else {
+            debit++
+        }
+    }
+    return {credit, debit};
+}
+
+createTransaction("credit", 50);
+createTransaction("credit", 120);
+createTransaction("debit", 80);
+createTransaction("debit", 30);
+
+console.log(user.balance); // 60
+
+console.log(getHigherTransactionByType("credit")); // { type: 'credit', value: 120 }
+console.log(getHigherTransactionByType("debit")); // { type: 'debit', value: 80 }
+
+console.log(getAverageTransactionValue()); // 70
+
+console.log(getTransactionsCount()); // { credit: 2, debit: 2 }
